5e36e4cc1ac4815aef11441cf44fde655447ece6
wilfriedlesauvage <obistrip@outlook.fr>
2020-05-14 12:12:04 +0200

    maj

--- SHIFUMI/projet/assets/js/script.js
+++ SHIFUMI/projet/assets/js/script.js
@@ -1,24 +1,40 @@
+/* -------------- Rejouer la partie ! ------------------------ */ 
+
+$('#refresh').on('click', function() {
+  location.reload();
+});
+
+
+
+var pierrej1 = $("#pierrej1").data('id');
+var feuillej1 = $("#feuille1").data('id');
+var ciseauxj1 = $("#ciseaux1").data('id');
+var shifumij1 = $("#shifumij1").data('id');
+
 $( function() {
-    $( "#pierrej1" ).draggable();
-    $( "shifumij1" ).droppable({
-      drop: function( event, ui ) {
-        $( this )
-          .addClass( "ui-state-highlight" )
-          .find( "p" )
-            .html( "Dropped!" );
-      }
+    $('#pierrej1').draggable({
+        revert : 'valid' // sera renvoyé à sa place s'il est déposé dans #drop
     });
 
+    $('#pierrej1').draggable({
+        revert : 'invalid' // sera renvoyé à sa place s'il n'est pas déposé dans #drop
+    });
 
+    $('#shifumij1').droppable({
 
+        drop : function(){
+            $("#pierrej1").data('id');
+            alert('pierre');
+        } 
 
+        if ()
 
-
-
-
-
-
-
+    });
 });
+
+
+
+
+
 
 
